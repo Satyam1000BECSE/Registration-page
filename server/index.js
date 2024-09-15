008e8e1a52d049f78ace21c389f50618afbce1c1
Satyam1000BECSE <128610133+Satyam1000BECSE@users.noreply.github.com>
2024-09-15 09:22:06 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,35 +1,31 @@
 const express = require("express")
 const mongoose = require('mongoose')
 const cors = require("cors")
-const EmployeeModel = require('./models/Employee')
+const RegisterModel = require('./models/Register')
 
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://localhost:27017/employee");
+mongoose.connect('mongodb://localhost:27017/employee');
 
-app.post("/login", (req, res) => {
-    const {email, password} = req.body;
-    EmployeeModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            if(user.password === password){
-                res.json("Success")
-            } else {
-                res.json("the password is incorrect")
-            }
-        }else{
-            res.json("No record existed")
-        }
-    })
+app.get("/", (req, res) =>{
+    res.json("Hello");
 })
 
-app.post('/register', (req, res) => {
-    EmployeeModel.create(req.body)
-    .then(employees => res.json(employees))
-    .catch(err => res.json(err))
+app.post("/register", (req, res) => {
+    const {name, email, password} = req.body;
+    RegisterModel.findOne({email: email})
+    .then(user => {
+        if(user){
+            res.json("Already have an account")
+        } else {
+            RegisterModel.create({name: name, email: email, password: password})
+            .then(result => res.json("Account created"))
+            .catch(err => res.json(err))
+        }
+    }).catch(err => res.json(err))
 })
 
 app.listen(3001, () => {
